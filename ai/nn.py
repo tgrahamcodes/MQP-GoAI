e268da76d5d59591cb2015b2622eae9fd2164749
Tom Graham <61550224+tgrahamcodes@users.noreply.github.com>
2020-10-13 12:30:34 -0400

    update nn

--- ai/nn.py
+++ ai/nn.py
@@ -1,14 +1,26 @@
+#-------------------------------------------------------------------------
 import torch
+import pickle
+import os
+from Players.minimax import MiniMaxPlayer
+from Players.mcts import MCTSPlayer 
+from Players.memory import MemoryDict
+from torch.utils.data import Dataset, DataLoader
+from pathlib import Path
+#-------------------------------------------------------------------------
 
 N, D_in, H, D_out = 64, 10, 5, 1
 
-## x = torch.randn(N, D_in)
-## y = torch.randn(N, D_out)
-
-with open('Players/Memory/MCTree.p', 'r') as f:
-    x = f.read()
-
+# x = torch.randn(N, D_in)
+# x = MemoryDict.fill_mem()
 y = torch.randn(N, D_out)
+
+filename = Path("Players/Memory/MCTree.p")
+
+
+with open(filename, "rb") as f:
+    x += f
+    ((x_train, y_train), (x_valid, y_valid), _) = pickle.load(x, encoding="utf-8")
 
 model = torch.nn.Sequential(
     torch.nn.Linear(D_in, H),
