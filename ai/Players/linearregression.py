0033fd03ffe0279fdf0b7287e51543d7dcf85fe4
Tom Graham <61550224+tgrahamcodes@users.noreply.github.com>
2020-11-03 12:25:00 -0500

    update

--- ai/Players/linearregression.py
+++ ai/Players/linearregression.py
@@ -12,6 +12,9 @@ class LinearRegression(nn.Module):
     def __init__(self, D_in, H, D_out, x1, y1):
         super(LinearRegression, self).__init__()
 
+        self.lin1 = nn.Linear(D_in, H)
+        self.lin2 = nn.Linear(H, D_out)
+
         xt = np.array([x1])
         yt = np.array([y1])
 
@@ -23,7 +26,7 @@ class LinearRegression(nn.Module):
 
         # Initializing the optimizer
         lr = 1e-4
-        opt = torch.optim.SGD(model.parameters(), lr)
+        # opt = torch.optim.SGD(model.parameters(), lr)
 
         epochs = 500
         losses=[]
@@ -31,25 +34,22 @@ class LinearRegression(nn.Module):
         # Actually training the model
         for i in range(epochs):
             # Compute y by passing x to the model
-            y_pred = model(x.float())
+            y_pred = self.lin2(x.float())
 
             loss = loss_fn(y_pred, y.float())
             if i % 100 == 99:
                 print(i, loss.item())
 
         # Zero the gradients before the backwards pass
-        model.zero_grad()
+        self.lin2.zero_grad()
 
         # Compute the loss gradient
         loss.backward()
 
         # Update the weights using gradient descent
         with torch.no_grad():
-            for i in model.parameters():
+            for i in self.lin2.parameters():
                 i += lr * i.grad
-
-        self.lin1 = nn.Linear(D_in, H)
-        self.lin2 = nn.Linear(H, D_out)
 
     def forward(self, X):
         h_relu = self.lin1(X).clamp(min=0)
--- ai/Tests/testlinearregression.py
+++ ai/Tests/testlinearregression.py
@@ -1,21 +1,19 @@
-from game import *
+# from game import *
 import numpy as np
 import sys
-from Players.mcts import *
-from Players.minimax import MiniMaxPlayer, GameState
-from Players.linearregression import *
-
+# from Players.mcts import *
+# from Players.minimax import MiniMaxPlayer, GameState
+from ..Players.linearregression import LinearRegression
 #-------------------------------------------------------------------------
 def test_python_version():
     assert sys.version_info[0]==3 # require python 3 (instead of python 2)
 #-------------------------------------------------------------------------
 
-
 #-------------------------------------------------------------------------
 def test_neural_net():
     # Set up batch size N, D_in is input,
     # H is hidden dimension , and D_out is output dimension
-    N, D_in, H, D_out = 64, 10, 100, 10
+    N, D_in, H, D_out = 64, 10, 10, 10
 
     # Hardcoded numpy arrays to test model "normalized"
     x1 = np.array([.1,.2,.3,.4,.5,.6,.7,.8,.9,.88])
@@ -23,7 +21,20 @@ def test_neural_net():
 
     # Instatiating the model
     model = LinearRegression(D_in, H, D_out, x1, y1)
+    assert (model != None)
+#-------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------
+def test_dict_split():
+    N, D_in, H, D_out = 64, 10, 5, 2
+
+    dict = {'0.2': 1, '0.3': 2, '0.4': 3, '0.5': 4, '0.6': 5}
+    result = dict.items()
+    data = list(result)
+    n = np.array(data, dtype=np.float16)
+
+    # Instatiating the model
+    model = LinearRegression(D_in, H, D_out, n, n)
 
     assert (model != None)
-    print(model)
 #-------------------------------------------------------------------------
