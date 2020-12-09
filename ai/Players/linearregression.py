ae51bbcc5ffb0fc211b47c4c149d25f947a21172
Tom Graham <61550224+tgrahamcodes@users.noreply.github.com>
2020-12-09 18:13:24 -0500

    added data_loader

--- ai/Players/linearregression.py
+++ ai/Players/linearregression.py
@@ -29,30 +29,50 @@ class LinearRegression(nn.Module):
         self.lin.load_state_dict(torch.load(file))
         self.lin.eval()
 
-    def train(self, x, y):
-        # Initializing the loss function
+    def train(self, data_loader, epochs=500):
+        loss_fn = nn.MSELoss()
+        optimizer = torch.optim.SGD(self.parameters(), lr=0.001, momentum=0.9)
+
+        for epoch in range(epochs):
+            running_loss = 0.0
+
+            for i, mini_batch in enumerate(data_loader):
+                states, labels = mini_batch
+                optimizer.zero_grad()
+                outputs = self(states)
+                loss = loss_fn(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+                running_loss += loss.item()
+
+                if i == (len(data_loader)-1):
+                    print('epoch %d: %.3f' % (epoch+1, running_loss/len(data_loader)))
+
+    # def train(self, x, y):
+    #     # Initializing the loss function
        
-        loss_fn = torch.nn.MSELoss(reduction='sum')
-        opt = torch.optim.SGD(self.parameters(), self.lr)
+    #     loss_fn = torch.nn.MSELoss(reduction='sum')
+    #     opt = torch.optim.SGD(self.parameters(), self.lr)
 
-        # Actually training the model
-        for epoch in range(self.epochs):
-            # Compute y by passing x to the model
+    #     # Actually training the model
+    #     for epoch in range(self.epochs):
+    #         # Compute y by passing x to the model
 
-            y_pred = self.forward(x)
+    #         y_pred = self.forward(x)
 
-            loss = loss_fn(y_pred, y)
+    #         loss = loss_fn(y_pred, y)
     
-            # Zero the gradients before the backwards pass
-            opt.zero_grad()
+    #         # Zero the gradients before the backwards pass
+    #         opt.zero_grad()
 
-            # Compute the loss gradient
-            loss.backward()
+    #         # Compute the loss gradient
+    #         loss.backward()
 
-            opt.step()
-            print('epoch ', epoch, 'loss ', loss.item())
+    #         opt.step()
+    #         print('epoch ', epoch, 'loss ', loss.item())
 
-            with torch.no_grad():
-                for param in self.parameters():
-                    param -= self.lr * param.grad
+    #         with torch.no_grad():
+    #             for param in self.parameters():
+    #                 param -= self.lr * param.grad
 # -------------------------------------------------------------------------
--- ai/Tests/testlinearregression.py
+++ ai/Tests/testlinearregression.py
@@ -8,53 +8,36 @@ import os.path
 from os import path
 from pathlib import Path
 from torch.autograd import Variable
-
+from torch.utils.data import DataLoader, Dataset
 #-------------------------------------------------------------------------
 def test_python_version():
     assert sys.version_info[0]==3 # require python 3 (instead of python 2)
 #-------------------------------------------------------------------------
 
-#-------------------------------------------------------------------------
-def test_neural_net():  
-    x_data = [i for i in range(11)]
-    y_data = [2*i + 1 for i in x_data]
+# #-------------------------------------------------------------------------
+# def test_neural_net():  
+#     x_data = [i for i in range(11)]
+#     y_data = [2*i + 1 for i in x_data]
 
-    x_train = np.array(x_data, dtype=np.float32)
-    x_train.shape
+#     x_train = np.array(x_data, dtype=np.float32)
+#     x_train.shape
 
-    y_train = np.array(y_data, dtype=np.float32)
-    y_train.shape
+#     y_train = np.array(y_data, dtype=np.float32)
+#     y_train.shape
 
-    x_train = x_train.reshape(-1, 1)
-    x_train.shape
+#     x_train = x_train.reshape(-1, 1)
+#     x_train.shape
 
-    y_train = y_train.reshape(-1, 1)
-    y_train.shape
+#     y_train = y_train.reshape(-1, 1)
+#     y_train.shape
 
-    inputs = torch.from_numpy(x_train).requires_grad_()
-    labels = torch.from_numpy(y_train)
+#     inputs = torch.from_numpy(x_train).requires_grad_()
+#     labels = torch.from_numpy(y_train)
 
-    # Instatiating the model
-    model = LinearRegression(1, 1)
-    print(model.train(inputs, labels))
-
-    ground_truth = Variable(torch.Tensor(([12])))
-    pred_y = model(ground_truth)
-    print("predict", 25, int(model(ground_truth).item()))
-    
-    # Asserting that the prediction is correct
-    assert (math.isclose(25, model(ground_truth).item(), abs_tol=1.5))
-
-    # Asserting the model is non null
-    assert (model and model.lin)
-
-    # Asserting the type of the model is correct
-    assert (type(model) == LinearRegression)
-
-    # Asserting the in and out features are correct
-    assert (model.lin.in_features == 1)
-    assert (model.lin.out_features == 1)
-#-------------------------------------------------------------------------
+#     # Instatiating the model
+#     model = LinearRegression(1, 1)
+#     print(model.train(inputs, labels))
+# #-------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------
 def test_data_save():
@@ -139,15 +122,17 @@ def test_data_save_and_load():
     newerModel.lin.weight = torch.nn.Parameter(torch.Tensor(4))
     newerModel.save('Players/Memory/MM_LinearReg2.p')
     
-    print("Model's state_dict:")
-    for param_tensor in newerModel.lin.state_dict():
-        print(param_tensor, "\t", newerModel.lin.state_dict()[param_tensor].size())
-
+   # print("Model's state_dict:")
+   # for param_tensor in newerModel.lin.state_dict():
+   #     print(param_tensor, "\t", newerModel.lin.state_dict()[param_tensor].size())
+  
     # Asserting the model has the same features as the loaded model
-    assert (model.lin.weight.data == newModel.lin.weight.data)
-    assert (model.lin.bias.data == newModel.lin.bias.data)
-    assert (model.lin.state_dict.__dict__ == newModel.lin.state_dict.__dict__)
-    # assert (newerModel.lin.state_dict()["weight"])
+    assert (torch.allclose(model.lin.bias, newModel.lin.bias))
+    assert (torch.allclose(model.lin.weight, newModel.lin.weight))
+
+    assert (len(newerModel.lin.weight.data) == 4)
+    assert (len(newerModel.lin.bias) == 2)
+
     # Asserting the objects are not null
     assert (model and model.lin)
     assert (newModel and newModel.lin)
@@ -160,3 +145,52 @@ def test_data_save_and_load():
     assert (path.exists('Players/Memory/MM_LinearReg.p'))
     assert Path(__file__).parents[1].joinpath('Players/Memory/MM_LinearReg.p')
 #-------------------------------------------------------------------------
+
+def test_neural_net():
+
+    x_data = [1,2,3]
+    y_data = [2*i + 1 for i in x_data]
+
+    x_train = np.array(x_data, dtype=np.float32)
+    x_train.shape
+
+    y_train = np.array(y_data, dtype=np.float32)
+    y_train.shape
+
+    x = torch.from_numpy(x_train).requires_grad_()
+    y = torch.from_numpy(y_train)
+
+    model = LinearRegression(1, 1)
+    model.file = 'Players/Memory/MM_LinearReg.p'
+
+    class sample_data(Dataset):
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+        def __len__(self):
+            return len(self.x) 
+        def __getitem__(self, index):
+            x = self.x[index]
+            y = self.y[index]
+            return x, y
+    d = sample_data(x, y)
+    data_loader = DataLoader(d, batch_size=1, shuffle=False, num_workers=0)
+    model.train(data_loader)
+
+    ground_truth = Variable(torch.Tensor(([12])))
+    pred_y = model(ground_truth)
+    print("predict", 25, int(model(ground_truth).item()))
+    
+    # Asserting that the prediction is correct
+    assert (math.isclose(25, model(ground_truth).item(), abs_tol=1.5))
+
+    # Asserting the model is non null
+    assert (model and model.lin)
+
+    # Asserting the type of the model is correct
+    assert (type(model) == LinearRegression)
+
+    # Asserting the in and out features are correct
+    assert (model.lin.in_features == 1)
+    assert (model.lin.out_features == 1)
+    #-------------------------------------------------------------------------
